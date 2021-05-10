adb83e2a458c78e9cf2257b27bb880940d2829ee
idCasper <fahrizasirojudin@gmail.com>
2021-05-11 02:30:56 +0700

    add modal delete in kios table

--- resources/views/kios/index.blade.php
+++ resources/views/kios/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.admin')
+
 @section('content')
 
   	<div class="container">
@@ -21,9 +22,10 @@
 			  <td>{{$item->user_id}}</td>
 			  <td>{{$item->verified}}</td>
 			  <td>{{$item->tgl_kontrak}}</td>
-			  <td><a href="{{url('/kios/'.$item->id.'/edit')}}" class="btn btn-success">Update</a>
-				<a class="btn btn-danger">Delete</a>
-			</td>
+			  <td>
+			  	<a href="{{url('/kios/'.$item->id.'/edit')}}" class="btn btn-success">Update</a>
+					<button type="button" data-toggle="modal" data-target="#delete-modal" class="btn btn-danger btn-delete" data-kios-id="{{ $item->id }}">Delete</button>
+				</td>
 			
 			</tr>
 			@endforeach
@@ -37,5 +39,40 @@
 		</div>
 	  
 	 </div>
+
+	<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog">
+	  <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title font-weight-bold text-danger">Warning</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <p>Are you sure to delete this post?</p>
+	      </div>
+	      <div class="modal-footer">
+	        <form id="delete-form" method="POST">
+	            @csrf
+	            @method('DELETE')
+
+	            <button class="btn btn-danger">Sure</button>
+	        </form>
+
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 	
-	 @endsection
+@endsection
+
+@push('custom_scripts')
+  <script type="text/javascript">
+		$(document).on('click', '.btn-delete', function () {
+	    var _id = $(this).data('kios-id');
+	    $('#delete-form').attr('action', '/kios/' + _id);
+		});
+	</script>
+@endpush
